2fc5dc0b70c05439396bc989af4278d207d19e5b
Tintu Mathew <55197440+tintupaulmathew@users.noreply.github.com>
2021-05-31 19:42:59 +0530

    made it more general

--- PrintStringPattern.java
+++ PrintStringPattern.java
@@ -1,25 +1,26 @@
 import java.io.*;
 class PrintingString{ 
 public static void main(String args[]){
-String str= "t&&1s#3d@@@7fw18", sub="", output="";
+String str= "t&&1s#03d@@@7fw18a003", sub="", output="", no="";
 str+='#';
-int no=0;
 for(int index=0; index<str.length(); index++){
 char c=str.charAt(index);
-if((c<48||c>57)&&no!=0){
-for(int k=0; k<no;k++)
+if((c<48||c>57)&&no!=""){
+int num=Integer.parseInt(no);
+if(num==0) sub="";
+for(int k=0; k<num;k++)
 output+=sub;
 sub=Character.toString(c); 
-no=0;
+no="";
 }
 else if(c>=48&&c<=57)
 {
-int num=Character.getNumericValue(c);
-no=no*10+num;
-if(no==0)sub="";
+no+=c;
 }
 else if(c<48||c>57)
 sub+=c;
 }
 System.out.println(output);
 }}
+
+
